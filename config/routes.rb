0c1f6284cf5e638b5d18e1312db3aefd248cd397
EC2 Default User <ec2-user@ip-172-31-22-105.us-east-2.compute.internal>
2020-04-20 09:58:00 +0000

    Create micropost system

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get "/signup", to: "users#new"
-  resources :users
   root "static_pages#home"
+  get 'sessions/new'
+  get "/signup", to: "users#new"
+  get '/users', to: 'users#show'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  
+  resources :users
+  resources :microposts,          only: [:create, :destroy]
 end
